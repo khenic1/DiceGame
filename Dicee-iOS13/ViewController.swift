f8d1aae6ae84615283e582d6a8c592eb06ddfd28
Kelley Henican <kelleyhenican2@gmail.com>
2020-01-27 10:13:06 -0800

    did second dice on load view and made roll button functional

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet var diceImageView2: UIImageView!
     @IBOutlet var diceImageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Who          What    Value
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
